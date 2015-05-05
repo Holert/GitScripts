4a2811b96bc550fa18b88a5f7450f675a88a1004
Johannes Holert <holert@mail.ubc.ca>
2015-05-05 16:29:25 -0700

    renamed sort_and_filter

---
+++ sort_and_filter_redundantHMMhits.py
@@ -0,0 +1,12 @@
+# Script for finding redundant HMM hits (proteins with more than one hit from different HMMs) in metagenomic datasets in filtered results datasets from HMM_search_and_parse_and_extract.py (Erick)
+
+# usage: 
+
+
+# sort files after protein ident
+
+# compare e-values in column 3
+
+# delete line with higher e-value
+
+# write out filtered file
