c9c922a74a4e3799561f93f9214a1d41a99e9711
Johannes Holert <holert@mail.ubc.ca>
2015-05-06 14:47:05 -0700

    Basic script (adapted from score_and_filter.hmm.results.py)

--- sort_and_filter_redundantHMMhits.py
+++ sort_and_filter_redundantHMMhits.py
@@ -1,12 +1,39 @@
-# Script for finding redundant HMM hits (proteins with more than one hit from different HMMs) in metagenomic datasets in filtered results datasets from HMM_search_and_parse_and_extract.py (Erick)
-
-# usage: 
+# Script for finding redundant HMM hits (proteins with more than one hit from the same HMM) in metagenomic datasets in filtered results datasets from HMM_search_and_parse_and_extract.py (Erick)
 
 
-# sort files after protein ident
+# usage: python sort_and_filter_redundantHMMhits.py <hmms.filtered.txt>
+# 				0									1
 
-# compare e-values in column 3
+import sys
 
-# delete line with higher e-value
+filein = open(sys.argv[1], 'r')
+outy = sys.argv[1]
+output = outy + '.clean.txt'
+fileout = open(output, 'w')
 
-# write out filtered file
+# create empty dictionary
+
+read_dictionary = {}
+
+# read lines in input file, split lines by Tab, extract protein ID, HMM and e-value
+
+for line in filein:
+    line1 = line.split('\t')
+    query = line1[0] # protein ID
+    subject = line1[1] # HMM
+    evalue = line1[2] # e-value
+    current_result = read_dictionary.get(query, [ 'none', 10])	# get e-value for protein ID
+    if evalue < current_result[1]: # compare e-values
+      	new_result =[subject, evalue]         
+        read_dictionary[query] = new_result		# update entry in dictionary
+    else:
+        continue
+        
+        
+#write dictionary
+
+for key, value in read_dictionary.iteritems():
+   fileout.write("%s\t%s\t%s\n" %(key, value[0], value[1]))
+
+fileout.close()
+filein.close()
