b01842093a054e0f06557745fba647379c00b312
Johannes Holert <holert@mail.ubc.ca>
2015-10-30 11:32:02 -0700

    create file

---
+++ Get_fasta_from_csv_to_one_file.py
@@ -0,0 +1,32 @@
+""" Gets AA sequence from csv or tsv files and stores it in fasta format"""
+
+# File created on 27 Oct 2015.
+
+# author__ = "Johannes Holert"
+
+# usage: python Get_fasta_from_csv_to_one_file.py 	<proteins.csv> 
+# 				0										1
+
+import sys
+import re
+
+
+filein = open(sys.argv[1], 'r')
+out1 = sys.argv[1] + '.faa'
+fileout = open(out1, 'w')
+
+# read lines in input file, split lines by Tab
+
+for line in filein:
+    if line.startswith('Filename'): # skips header line in results files
+        continue
+    else:
+	    line2 = line.split(',')
+	    Filename = line2[0] # contigID
+	    proteinID = line2[1] # coverage
+	    seq = line2[12] # create dictionary key = contig_ID
+	    fileout.write(">%s_%s\n%s" %(Filename, proteinID, seq))
+
+
+fileout.close()
+filein.close()
