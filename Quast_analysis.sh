ce1e3728c982f4440df975650b469a873333b821
Johannes Holert <holert@mail.ubc.ca>
2016-03-10 14:25:04 -0800

    run quast script

---
+++ Quast_analysis.sh
@@ -0,0 +1,3 @@
+# quast analysis of *.fna metagenome sequences
+
+for i in *.fna; do python /home/data/metagenome_collection/quast-3.0/metaquast.py --no-plots --no-html --no-snps --max-ref-number 0 --min-contig 10 --threads 10 $i;rm quast_results/*/quast_corrected_input/*;done
