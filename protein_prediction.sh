db6a7a726285a2a869ad876e31ba0583e2f7e593
Johannes Holert <Jojo@Johanness-MacBook-Pro.local>
2016-09-23 13:27:56 -0700

    changed input file type

--- protein_prediction.sh
+++ protein_prediction.sh
@@ -1,2 +1,2 @@
 #for i in `ls *fasta;do echo $i;prodigal -a $i.faa -i $i -m -o $i.txt -p meta -q;done
-parallel --gnu -j10 'prodigal -a {.}.faa -i {} -m -o /dev/null -p meta -q' ::: *fa
+parallel --gnu -j10 'prodigal -a {.}.faa -i {} -m -o /dev/null -p meta -q' ::: *fasta
